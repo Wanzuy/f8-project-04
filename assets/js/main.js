78c74d5f9139ee6bb18d6b2c303fe56c0c091069
Duy Quang <qn247732@gmail.com>
2024-06-13 22:40:40 +0700

    blog

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,6 @@ const $$ = document.querySelectorAll.bind(document);
 
 const tabs = $$(".tab__item");
 const tabcontents = $$(".tab__content-item");
-
 tabs.forEach((tab, index) => {
     const tabcontent = tabcontents[index];
 
@@ -15,4 +14,53 @@ tabs.forEach((tab, index) => {
         this.classList.add("tab__item--active");
         tabcontent.classList.add("tab__content-item--active");
     };
+});
+
+const dots = $$(".review__dot");
+const reviewlist = $(".review__list");
+const reviewItems = $$(".review-item");
+const btnright = $(".btn-right");
+const btnleft = $(".btn-left");
+const leng = reviewItems.length;
+let current = 0;
+
+const handleChang = () => {
+    if (current == leng - 1) {
+        current = 0;
+        reviewlist.style.transform = `translateX(0)`;
+        $(".review__dot.review__dot--active").classList.remove(
+            "review__dot--active"
+        );
+        dots[current].classList.add("review__dot--active");
+    } else {
+        current++;
+        reviewlist.style.transform = `translateX(-${current * 100}%)`;
+        $(".review__dot.review__dot--active").classList.remove(
+            "review__dot--active"
+        );
+        dots[current].classList.add("review__dot--active");
+        console.log(current);
+    }
+};
+
+btnright.addEventListener("click", () => {
+    handleChang();
+});
+
+btnleft.addEventListener("click", () => {
+    if (current == 0) {
+        current = leng - 1;
+        reviewlist.style.transform = `translateX(-${current * 100}%)`;
+        $(".review__dot.review__dot--active").classList.remove(
+            "review__dot--active"
+        );
+        dots[current].classList.add("review__dot--active");
+    } else {
+        current--;
+        reviewlist.style.transform = `translateX(-${current * 100}%)`;
+        $(".review__dot.review__dot--active").classList.remove(
+            "review__dot--active"
+        );
+        dots[current].classList.add("review__dot--active");
+    }
 });
